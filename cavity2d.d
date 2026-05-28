45e379b16b530719c8c21c18e744895c1db663c6
Predrag Tekic <predrag.tekic@gmail.com>
2026-05-28 16:31:12 +0200

    code commit

---
+++ cavity2d.d
@@ -0,0 +1,307 @@
+/home/MICOS-MACOS/olb-0.5r1/examples/cavity2d/cavity2d.o: cavity2d.cpp ../../src/olb2D.h \
+  ../../src/complexGrids/mpiManager/mpiManager.h ../../src/core/core2D.h \
+  ../../src/core/olbInit.h ../../src/io/parallelIO.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/streambuf \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/c++config.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/os_defines.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/iosfwd \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/c++locale.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/clocale \
+  /usr/include/locale.h /usr/include/_ansi.h /usr/include/newlib.h \
+  /usr/include/sys/config.h /usr/include/machine/ieeefp.h \
+  /usr/include/cygwin/config.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cstring \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cstddef \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/stddef.h \
+  /usr/include/string.h /usr/include/sys/reent.h /usr/include/_ansi.h \
+  /usr/include/sys/_types.h /usr/include/sys/lock.h \
+  /usr/include/sys/string.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cstdio \
+  /usr/include/stdio.h /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/stdarg.h \
+  /usr/include/sys/types.h /usr/include/machine/_types.h \
+  /usr/include/machine/types.h /usr/include/sys/features.h \
+  /usr/include/cygwin/types.h /usr/include/sys/sysmacros.h \
+  /usr/include/stdint.h /usr/include/endian.h /usr/include/sys/stdio.h \
+  /usr/include/sys/cdefs.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/c++io.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/gthr.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/gthr-default.h \
+  /usr/include/pthread.h /usr/include/signal.h /usr/include/sys/signal.h \
+  /usr/include/cygwin/signal.h /usr/include/sched.h /usr/include/time.h \
+  /usr/include/machine/time.h /usr/include/cygwin/time.h \
+  /usr/include/unistd.h /usr/include/sys/unistd.h /usr/include/getopt.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cctype \
+  /usr/include/ctype.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stringfwd.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/postypes.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cwchar \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/ctime \
+  /usr/include/wchar.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/functexcept.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/exception_defines.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/localefwd.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/ios_base.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/atomicity.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/atomic_word.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/locale_classes.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/string \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/char_traits.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_algobase.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/climits \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/limits.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/syslimits.h \
+  /usr/include/limits.h /usr/include/features.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cstdlib \
+  /usr/include/stdlib.h /usr/include/machine/stdlib.h \
+  /usr/include/alloca.h /usr/include/cygwin/stdlib.h \
+  /usr/include/cygwin/wait.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/new \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/exception \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_pair.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/type_traits.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_iterator_base_types.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_iterator_base_funcs.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/concept_check.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_iterator.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/debug/debug.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cassert \
+  /usr/include/assert.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/memory \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/allocator.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/c++allocator.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/ext/new_allocator.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_construct.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_uninitialized.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_raw_storage_iter.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/limits \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_function.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/basic_string.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/algorithm \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_algo.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_heap.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_tempbuf.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/basic_string.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/streambuf.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/istream \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/ios \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/basic_ios.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/streambuf_iterator.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/locale_facets.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cwctype \
+  /usr/include/wctype.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/ctype_base.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/ctype_inline.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/codecvt.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/time_members.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/messages_members.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/basic_ios.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/istream.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/locale \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/locale_facets.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/typeinfo \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/ostream \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/ostream.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/fstream \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/basic_file.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/fstream.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/iostream \
+  ../../src/core/ompManager.h ../../src/core/multiPhysics.h \
+  ../../src/core/singleton.h ../../src/core/units.h \
+  ../../src/core/latticeDescriptors.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/vector \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_vector.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_bvector.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/vector.tcc \
+  ../../src/core/olbDebug.h ../../src/core/dynamics.h \
+  ../../src/core/util.h ../../src/core/postProcessing.h \
+  ../../src/core/spatiallyExtendedObject2D.h \
+  ../../src/complexGrids/multiBlockStructure/multiDataGeometry2D.h \
+  ../../src/core/util.h ../../src/core/spatiallyExtendedObject3D.h \
+  ../../src/complexGrids/multiBlockStructure/multiDataGeometry3D.h \
+  ../../src/core/cell.h ../../src/core/momentaOnBoundaries.h \
+  ../../src/core/momentaOnBoundaries2D.h ../../src/core/dataFieldBase2D.h \
+  ../../src/core/serializer.h ../../src/core/dataReductions.h \
+  ../../src/core/dataFields2D.h ../../src/core/blockStructure2D.h \
+  ../../src/core/dataAnalysisBase2D.h \
+  ../../src/core/boundaryPostProcessors2D.h \
+  ../../src/core/blockLattice2D.h ../../src/core/blockLatticeView2D.h \
+  ../../src/core/boundaryCondition2D.h ../../src/core/simulationSetup2D.h \
+  ../../src/core/loadBalancer.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/map \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_tree.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/cpp_type_traits.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_map.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_multimap.h \
+  ../../src/core/analysisFlags.h ../../src/core/dataAnalysis2D.h \
+  ../../src/boundaryConditions/boundaryConditions2D.h \
+  ../../src/boundaryConditions/extendedFiniteDifferenceBoundary2D.h \
+  ../../src/core/postProcessing.h ../../src/core/momentaOnBoundaries.h \
+  ../../src/core/blockLattice2D.h ../../src/core/boundaryCondition2D.h \
+  ../../src/boundaryConditions/inamuroAnalyticalDynamics.h \
+  ../../src/core/dynamics.h \
+  ../../src/boundaryConditions/inamuroBoundary2D.h \
+  ../../src/boundaryConditions/inamuroNewtonRaphsonDynamics.h \
+  ../../src/boundaryConditions/zouHeBoundary2D.h \
+  ../../src/boundaryConditions/zouHeDynamics.h \
+  ../../src/complexDynamics/complexDynamics2D.h \
+  ../../src/complexDynamics/mrtLatticeDescriptors.h \
+  ../../src/core/latticeDescriptors.h \
+  ../../src/complexDynamics/advectionDiffusionLatticeDescriptors.h \
+  ../../src/complexDynamics/forcedShanChenLatticeDescriptors.h \
+  ../../src/complexDynamics/entropicDynamics.h ../../src/core/dynamics.h \
+  ../../src/complexDynamics/entropicLbHelpers.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cmath /usr/include/math.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/cmath.tcc \
+  ../../src/complexDynamics/entropicLbHelpers2D.h \
+  ../../src/complexDynamics/entropicLbHelpers3D.h \
+  ../../src/complexDynamics/mrtDynamics.h \
+  ../../src/complexDynamics/chopardDynamics.h ../../src/core/cell.h \
+  ../../src/complexDynamics/navierStokesAdvectionDiffusionCouplingPostProcessor2D.h \
+  ../../src/core/spatiallyExtendedObject2D.h \
+  ../../src/core/postProcessing.h ../../src/core/blockLattice2D.h \
+  ../../src/complexDynamics/advectionDiffusionDynamics.h \
+  ../../src/complexDynamics/advectionDiffusionBoundaries.h \
+  ../../src/complexDynamics/advectionDiffusionBoundaryCondition2D.h \
+  ../../src/core/momentaOnBoundaries2D.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/list \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_list.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/list.tcc \
+  ../../src/complexDynamics/advectionDiffusionMomenta.h \
+  ../../src/complexDynamics/advectionDiffusionUnits.h \
+  ../../src/core/singleton.h \
+  ../../src/complexDynamics/forcedShanChenCouplingPostProcessor2D.h \
+  ../../src/io/io2D.h ../../src/io/base64.h ../../src/io/serializerIO.h \
+  ../../src/core/serializer.h ../../src/core/dataFieldBase2D.h \
+  ../../src/core/dataFieldBase3D.h ../../src/core/blockStructure2D.h \
+  ../../src/core/blockStructure3D.h ../../src/core/dataFields3D.h \
+  ../../src/core/dataFieldBase3D.h ../../src/core/dataAnalysisBase3D.h \
+  ../../src/io/vtkDataOutput.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/sstream \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/sstream.tcc \
+  ../../src/io/parallelIO.h ../../src/io/colormaps.h \
+  ../../src/io/imageWriter.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/iomanip \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/functional \
+  ../../src/utilities/utilities2D.h ../../src/utilities/benchmarkUtil.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/deque \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_deque.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/deque.tcc \
+  ../../src/utilities/olbAlgorithms.h \
+  ../../src/complexGrids/cuboidStructure/cuboidStructure2D.h \
+  ../../src/complexGrids/cuboidStructure/cuboid2D.h \
+  ../../src/complexGrids/cuboidStructure/cuboidGeometry2D.h \
+  ../../src/complexGrids/cuboidStructure/cuboidNeighbourhood2D.h \
+  ../../src/complexGrids/cuboidStructure/superLattice2D.h \
+  ../../src/core/blockLattice2D.h ../../src/core/blockLatticeView2D.h \
+  ../../src/complexGrids/cuboidStructure/communicator2D.h \
+  ../../src/core/loadBalancer.h ../../src/core/postProcessing.h \
+  ../../src/complexGrids/cuboidStructure/superBoundaryCondition2D.h \
+  ../../src/core/boundaryCondition2D.h \
+  ../../src/complexGrids/cuboidStructure/cuboidVTKout.h \
+  ../../src/core/dataFields2D.h ../../src/core/dataFields3D.h \
+  ../../src/complexGrids/cuboidStructure/cuboidGeometry3D.h \
+  ../../src/complexGrids/cuboidStructure/cuboid3D.h \
+  ../../src/complexGrids/multiBlockStructure/multiBlockStructure2D.h \
+  ../../src/complexGrids/multiBlockStructure/multiSerializer2D.h \
+  ../../src/core/serializer.h \
+  ../../src/complexGrids/multiBlockStructure/multiDataGeometry2D.h \
+  ../../src/complexGrids/multiBlockStructure/multiBlockLattice2D.h \
+  ../../src/core/blockLattice2D.h ../../src/core/cell.h \
+  ../../src/core/dynamics.h \
+  ../../src/complexGrids/multiBlockStructure/multiBlockStatistics.h \
+  ../../src/core/postProcessing.h \
+  ../../src/complexGrids/multiBlockStructure/multiBlockHandler2D.h \
+  ../../src/core/dataAnalysisBase2D.h \
+  ../../src/complexGrids/multiBlockStructure/parallelDynamics.h \
+  ../../src/complexGrids/multiBlockStructure/multiDataFieldHandler2D.h \
+  ../../src/complexGrids/multiBlockStructure/multiDataGeometry2D.h \
+  ../../src/complexGrids/multiBlockStructure/multiDataFields2D.h \
+  ../../src/core/olbDebug.h ../../src/core/dataFieldBase2D.h \
+  ../../src/core/dataFields2D.h \
+  ../../src/complexGrids/multiBlockStructure/multiSerializer2D.h \
+  ../../src/complexGrids/multiBlockStructure/multiDataUtilities2D.h \
+  ../../src/complexGrids/multiBlockStructure/multiDataAnalysis2D.h \
+  ../../src/core/dataAnalysis2D.h \
+  ../../src/complexGrids/multiBlockStructure/multiBlockLattice2D.h \
+  ../../src/olb2D.hh ../../src/core/core2D.hh \
+  ../../src/core/latticeDescriptors.hh ../../src/core/cell.hh \
+  ../../src/core/dynamics.hh ../../src/core/lbHelpers.h \
+  ../../src/core/lbHelpers2D.h ../../src/core/lbHelpers3D.h \
+  ../../src/core/firstOrderLbHelpers.h \
+  ../../src/core/firstOrderLbHelpers2D.h \
+  ../../src/core/firstOrderLbHelpers3D.h ../../src/core/d3q13Helpers.h \
+  ../../src/core/momentaOnBoundaries.hh \
+  ../../src/core/momentaOnBoundaries2D.hh \
+  ../../src/core/postProcessing.hh ../../src/core/blockLattice3D.h \
+  ../../src/core/blockStructure3D.h ../../src/core/dataFields2D.hh \
+  ../../src/core/serializer.hh ../../src/core/boundaryPostProcessors2D.hh \
+  ../../src/core/finiteDifference2D.h ../../src/core/finiteDifference.h \
+  ../../src/core/blockLattice2D.hh ../../src/core/blockLatticeView2D.hh \
+  ../../src/core/boundaryCondition2D.hh \
+  ../../src/core/boundaryInstantiator2D.h \
+  ../../src/core/simulationSetup2D.hh ../../src/core/dataAnalysis2D.hh \
+  ../../src/core/dataReductions.hh \
+  ../../src/boundaryConditions/boundaryConditions2D.hh \
+  ../../src/boundaryConditions/extendedFiniteDifferenceBoundary2D.hh \
+  ../../src/core/finiteDifference2D.h ../../src/core/util.h \
+  ../../src/core/lbHelpers.h ../../src/core/firstOrderLbHelpers.h \
+  ../../src/core/boundaryInstantiator2D.h \
+  ../../src/boundaryConditions/inamuroAnalyticalDynamics.hh \
+  ../../src/core/latticeDescriptors.h \
+  ../../src/boundaryConditions/inamuroBoundary2D.hh \
+  ../../src/boundaryConditions/inamuroNewtonRaphsonDynamics.hh \
+  ../../src/boundaryConditions/zouHeBoundary2D.hh \
+  ../../src/boundaryConditions/zouHeDynamics.hh \
+  ../../src/complexDynamics/complexDynamics2D.hh \
+  ../../src/complexDynamics/mrtLatticeDescriptors.hh \
+  ../../src/complexDynamics/advectionDiffusionLatticeDescriptors.hh \
+  ../../src/complexDynamics/forcedShanChenLatticeDescriptors.hh \
+  ../../src/complexDynamics/entropicDynamics.hh \
+  ../../src/core/lbHelpers.h ../../src/complexDynamics/mrtDynamics.hh \
+  ../../src/complexDynamics/mrtHelpers.h \
+  ../../src/complexDynamics/mrtHelpers2D.h \
+  ../../src/complexDynamics/mrtHelpers3D.h \
+  ../../src/complexDynamics/chopardDynamics.hh ../../src/core/util.h \
+  ../../src/complexDynamics/navierStokesAdvectionDiffusionCouplingPostProcessor2D.hh \
+  ../../src/core/finiteDifference2D.h \
+  ../../src/complexDynamics/advectionDiffusionDynamics.hh \
+  ../../src/complexDynamics/advectionDiffusionLbHelpers.h \
+  ../../src/complexDynamics/advectionDiffusionLbHelpers2D.h \
+  ../../src/complexDynamics/advectionDiffusionLbHelpers3D.h \
+  ../../src/complexDynamics/advectionDiffusionBoundaries.hh \
+  ../../src/complexDynamics/utilAdvectionDiffusion.h \
+  ../../src/complexDynamics/advectionDiffusionBoundaryCondition2D.hh \
+  ../../src/complexDynamics/advectionDiffusionBoundaryInstantiator2D.h \
+  ../../src/complexDynamics/advectionDiffusionMomenta.hh \
+  ../../src/complexDynamics/forcedShanChenCouplingPostProcessor2D.hh \
+  ../../src/io/io2D.hh ../../src/io/base64.hh ../../src/core/olbDebug.h \
+  ../../src/io/serializerIO.hh \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/numeric \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/iterator \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stream_iterator.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_numeric.h \
+  ../../src/io/vtkDataOutput.hh ../../src/core/singleton.h \
+  ../../src/io/colormaps.hh ../../src/io/imageWriter.hh \
+  ../../src/core/dataFields2D.h ../../src/utilities/utilities2D.hh \
+  ../../src/utilities/benchmarkUtil.hh \
+  ../../src/complexGrids/cuboidStructure/cuboidStructure2D.hh \
+  ../../src/complexGrids/cuboidStructure/cuboid2D.hh \
+  ../../src/core/lbHelpers.h \
+  ../../src/complexGrids/cuboidStructure/cuboidGeometry2D.hh \
+  ../../src/complexGrids/cuboidStructure/cuboidNeighbourhood2D.hh \
+  ../../src/core/dynamics.h ../../src/core/cell.h \
+  ../../src/complexGrids/cuboidStructure/communicator2D.hh \
+  ../../src/complexGrids/cuboidStructure/superBoundaryCondition2D.hh \
+  ../../src/complexGrids/cuboidStructure/superLattice2D.hh \
+  ../../src/complexGrids/cuboidStructure/cuboidVTKout.hh \
+  ../../src/core/singleton.h ../../src/core/dataFields3D.hh \
+  ../../src/complexGrids/multiBlockStructure/multiBlockStructure2D.hh \
+  ../../src/complexGrids/multiBlockStructure/multiSerializer2D.hh \
+  ../../src/complexGrids/multiBlockStructure/multiBlockLattice2D.hh \
+  ../../src/complexGrids/multiBlockStructure/multiBlockStatistics.hh \
+  ../../src/complexGrids/multiBlockStructure/multiBlockHandler2D.hh \
+  ../../src/complexGrids/multiBlockStructure/parallelDynamics.hh \
+  ../../src/complexGrids/multiBlockStructure/multiDataFieldHandler2D.hh \
+  ../../src/complexGrids/multiBlockStructure/parallelDynamics.h \
+  ../../src/complexGrids/multiBlockStructure/multiDataFields2D.hh \
+  ../../src/complexGrids/multiBlockStructure/multiDataAnalysis2D.hh
